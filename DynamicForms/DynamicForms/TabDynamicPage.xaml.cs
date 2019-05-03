224be7adca07e33a84295c0bd25283272b97813d
fmoreno24 <fabiin.andree@gmail.com>
2019-05-03 18:05:06 -0500

    fmoreno
    Se inicia dynamic forms demo

---
+++ DynamicForms/DynamicForms/TabDynamicPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿
+namespace DynamicForms
+{
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TabDynamicPage : TabbedPage
+	{
+		public TabDynamicPage ()
+		{
+			InitializeComponent ();
+            createPage();
+        }
+
+        private void createPage()
+        {
+            var PageDynamic = new ContentPage();
+            var label = new Label();
+            label.Text = "Fuck yeah!!!!!!!";
+
+            var ContentStack = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                Children =
+                {
+                    label
+                }
+            };
+
+            PageDynamic.Title = "Fuck";
+            PageDynamic.Content = ContentStack;
+            this.Children.Add(PageDynamic);
+
+            var PageDynamic2 = new ContentPage();
+            var label2 = new Label();
+            label2.Text = "Fuck yeah!!!!!!! 2";
+
+            var ContentStack2 = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                Children =
+                {
+                    label2
+                }
+            };
+
+            PageDynamic2.Title = "Fuck2";
+            PageDynamic2.Content = ContentStack2;
+
+            
+            this.Children.Add(PageDynamic2);
+        }
+    }
+}
---
+++ DynamicForms/DynamicForms/TabPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DynamicForms
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabPage : TabbedPage
+    {
+        public TabPage ()
+        {
+            InitializeComponent();
+            createPage();
+        }
+
+        private void createPage()
+        {
+            var PageDynamic = new ContentPage();
+            var label = new Label();
+            label.Text = "Fuck yeah!!!!!!!";
+
+            var ContentStack = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                Children =
+                {
+                    label
+                }
+            };
+
+            PageDynamic.Title = "Fuck";
+            PageDynamic.Content = ContentStack;
+
+            this.Children.Add(PageDynamic);
+        }
+    }
+}
